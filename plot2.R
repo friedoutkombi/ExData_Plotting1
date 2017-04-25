32d9126d3d016febc724895501db1b598ad8e3ee
friedoutkombi <nicholastdyer@gmail.com>
2017-04-25 17:15:08 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,34 +1,21 @@
-#This R code will be for the creation of Plot 1 - Global Active Power.
-#Basic steps are load in the data set.
-#Analyse the data to get a feel for it.
-#Cut down to the 2 day time period.
-#Plot Global Active Power vs Frequency.
-#Ensure labels, colours are good.
+#This R code will be for the creation of Plot 2 - 
 
 
 #Load in the data
-df <- read.table("household_power_consumption.txt",header = TRUE, sep=";")
+df <- read.table("household_power_consumption.txt",header = TRUE, sep=";",stringsAsFactors=FALSE,dec=".")
+
 
 #Cull data down to the two days relevant to the time period
 twodaydata <- subset(df, Date %in% c("1/2/2007","2/2/2007"))
 
-#Convert Date  to appropriate class (Date)
-twodaydata$Date<-as.Date(twodaydata$Date, format="%d/%m/%Y")
-
-#Convert Global Active Power to Numeric
-twodaydata$Global_active_power<-as.numeric(as.character(twodaydata$Global_active_power))
-
-#Convert to kilowatts
-twodaydata$Global_active_power<-twodaydata$Global_active_power
-#Create a histogram with the 
-hist(twodaydata$Global_active_power,main="Global Active Power",col="Red",
-     xlab="Global Active Power (kilowatts)")
+#convert date/time structures
+datetime<-strptime(paste(twodaydata$Date, twodaydata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 
 #Export to png format
-png("plot1.png", width=480, height=480)
+png("plot2.png", width=480, height=480)
 
-#Create a histogram with the 
-hist(twodaydata$Global_active_power,main="Global Active Power",col="Red",
-     xlab="Global Active Power (kilowatts)")
+#plot
+plot(datetime,twodaydata$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+
 #Close Window
 dev.off()
