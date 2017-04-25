3346e57d1c638b3cda0a44ac1e29cd208a0a54a5
friedoutkombi <nicholastdyer@gmail.com>
2017-04-25 15:26:29 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -12,5 +12,23 @@ df <- read.table("household_power_consumption.txt",header = TRUE, sep=";")
 #Cull data down to the two days relevant to the time period
 twodaydata <- subset(df, Date %in% c("1/2/2007","2/2/2007"))
 
-#Convert Date & Time to appropriate class (factor to Date/Time)
+#Convert Date  to appropriate class (Date)
 twodaydata$Date<-as.Date(twodaydata$Date, format="%d/%m/%Y")
+
+#Convert Global Active Power to Numeric
+twodaydata$Global_active_power<-as.numeric(as.character(twodaydata$Global_active_power))
+
+#Convert to kilowatts
+twodaydata$Global_active_power<-twodaydata$Global_active_power
+#Create a histogram with the 
+hist(twodaydata$Global_active_power,main="Global Active Power",col="Red",
+     xlab="Global Active Power (kilowatts)")
+
+#Export to png format
+png("plot1.png", width=480, height=480)
+
+#Create a histogram with the 
+hist(twodaydata$Global_active_power,main="Global Active Power",col="Red",
+     xlab="Global Active Power (kilowatts)")
+#Close Window
+dev.off()
